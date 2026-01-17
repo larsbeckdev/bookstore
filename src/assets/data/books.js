5b5aaadbef5f1dea70a950ad76e24e8781d04d62
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-17 15:09:51 +0100

    add: books & book template

---
+++ src/assets/data/books.js
@@ -0,0 +1,212 @@
+let books = [
+  // Book 1
+  {
+    name: "Die Geheimnisse des Ozeans",
+    author: "Clara Meer",
+    cover: "./src/assets/images/die-geheimnisse-des-ozeans.png",
+    likes: 1250,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2018,
+    genre: "Fantasy",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein faszinierendes Abenteuerbuch, das mich von der ersten Seite an gefesselt hat.",
+      },
+      {
+        name: "Bookworm84",
+        comment:
+          "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat.",
+      },
+      {
+        name: "FantasyFanatic",
+        comment:
+          "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte.",
+      },
+      {
+        name: "SciFiGuru",
+        comment:
+          "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren.",
+      },
+      {
+        name: "NovelLover",
+        comment:
+          "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat.",
+      },
+    ],
+  },
+  // Book 2
+  {
+    name: "Der vergessene Pfad",
+    author: "Maximilian Schwarz",
+    cover: "./src/assets/images/der-vergessene-pfad.png",
+    likes: 980,
+    liked: false,
+    price: 14.5,
+    publishedYear: 2021,
+    genre: "Fantasy",
+    comments: [],
+  },
+  // Book 3
+  {
+    name: "Die Farben des Himmels",
+    author: "Laura Blau",
+    cover: "./src/assets/images/die-farben-des-himmels.png",
+    likes: 1520,
+    liked: true,
+    price: 22.95,
+    publishedYear: 2019,
+    genre: "Romantik",
+    comments: [
+      {
+        name: "LeserPeter",
+        comment:
+          "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt.",
+      },
+      {
+        name: "BookLover21",
+        comment:
+          "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat.",
+      },
+      {
+        name: "FantasyNerd",
+        comment:
+          "Fantastische Welten und epische Abenteuer - genau mein Geschmack!",
+      },
+      {
+        name: "SciFiEnthusiast",
+        comment:
+          "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht.",
+      },
+      {
+        name: "ReadingAddict",
+        comment:
+          "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat.",
+      },
+    ],
+  },
+  // Book 4
+  {
+    name: "Das Rätsel der Zeit",
+    author: "Alexander Weiss",
+    cover: "./src/assets/images/das-raetsel-der-zeit.png",
+    likes: 750,
+    liked: false,
+    price: 18.0,
+    publishedYear: 2020,
+    genre: "Science-Fiction",
+    comments: [
+      {
+        name: "BuchKenner",
+        comment:
+          "Ein spannendes Abenteuer, das mich von Anfang an mitgerissen hat.",
+      },
+      {
+        name: "LeseWurm",
+        comment:
+          "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben.",
+      },
+    ],
+  },
+  // Book 5
+  {
+    name: "Der letzte Wächter",
+    author: "Sabine Grün",
+    cover: "./src/assets/images/der-letzte-waechter.png",
+    likes: 1300,
+    liked: true,
+    price: 16.75,
+    publishedYear: 2017,
+    genre: "Fantasy",
+    comments: [],
+  },
+  // Book 6
+  {
+    name: "Im Schatten des Mondes",
+    author: "Philipp Silber",
+    cover: "./src/assets/images/im-schatten-des-mondes.png",
+    likes: 890,
+    liked: false,
+    price: 12.3,
+    publishedYear: 2022,
+    genre: "Science-Fiction",
+    comments: [
+      {
+        name: "BücherLiebhaber",
+        comment:
+          "Eine magische Reise durch eine faszinierende Fantasiewelt, absolut fesselnd.",
+      },
+      {
+        name: "Leseratte",
+        comment:
+          "Ein packender Science-Fiction-Roman, der mich zum Nachdenken gebracht hat.",
+      },
+    ],
+  },
+  // Book 7
+  {
+    name: "Jenseits der Sterne",
+    author: "Oliver Schwarz",
+    cover: "./src/assets/images/jenseits-der-sterne.png",
+    likes: 1450,
+    liked: true,
+    price: 21.0,
+    publishedYear: 2015,
+    genre: "Science-Fiction",
+    comments: [
+      {
+        name: "Leser123",
+        comment:
+          "Ein fesselndes Abenteuer, das mich von Anfang bis Ende mitgerissen hat.",
+      },
+    ],
+  },
+  // Book 8
+  {
+    name: "Das verborgene Königreich",
+    author: "Elena Gold",
+    cover: "./src/assets/images/das-verborgene-koenigreich.png",
+    likes: 920,
+    liked: false,
+    price: 17.5,
+    publishedYear: 2020,
+    genre: "Fantasy",
+    comments: [
+      {
+        name: "Bookworm92",
+        comment:
+          "Ein faszinierendes Buch, das mich von der ersten Seite an gefesselt hat.",
+      },
+    ],
+  },
+  // Book 9
+  {
+    name: "Liebe in Zeiten des Krieges",
+    author: "Emilia Rot",
+    cover: "./src/assets/images/liebe-in-zeiten-des-krieges.png",
+    likes: 1800,
+    liked: true,
+    price: 19.99,
+    publishedYear: 2016,
+    genre: "Romantik",
+    comments: [
+      {
+        name: "Bibliophile23",
+        comment:
+          "Die Fantasiewelt war so lebendig, ich konnte das Buch kaum aus der Hand legen.",
+      },
+      {
+        name: "StorySeeker",
+        comment:
+          "Eine unglaublich berührende Liebesgeschichte, die mich tief bewegt hat.",
+      },
+      {
+        name: "SciFiExplorer",
+        comment:
+          "Spannende Zukunftsvisionen und interessante Charaktere machten diesen Roman einzigartig.",
+      },
+    ],
+  },
+];
---
+++ src/assets/js/book-list.js
@@ -0,0 +1,86 @@
+const listEl = document.getElementById("book-card-list");
+
+function renderBooks() {
+  listEl.innerHTML = "";
+
+  for (let i = 0; i < books.length; i++) {
+    listEl.innerHTML += bookCard(books[i], i);
+  }
+}
+
+// Book Card Template
+function bookCard(book, index) {
+  return `
+    <div class="book-card">
+
+      <div class="book-card-header">
+        <div class="book-card-header-heading">
+          <h3>${book.name}</h3>
+          <p>${book.author}</p>
+        </div>
+
+        <div class="like-container">
+          <button
+            onclick="toggleLike(${index})"
+            class="like-btn ${book.liked ? "liked" : ""}"
+          >
+            <span>${book.likes}</span>
+            ${heartSvg()}
+          </button>
+        </div>
+      </div>
+
+
+      <div class="book-card-main">
+        <div class="book-cover-container">
+          <img
+            src="${
+              book.cover ? book.cover : "./src/assets/images/placeholder.png"
+            }"
+            alt="Buchcover von ${book.name}"
+            class="book-cover"
+          />
+        </div>
+
+         <p>Kommentare: ${book.publishedYear}</p>
+        <p>Kommentare: ${book.comments.length}</p>
+
+
+      </div>
+
+      <div class="book-card-footer">
+      <button class="add-to-cart-btn">In den Warenkorb</button>
+      </div>
+
+    </div>
+  `;
+}
+
+// Like Button Heart SVG
+function heartSvg() {
+  return `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      class="heart-icon"
+      aria-hidden="true"
+    >
+      <path
+        d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"
+      />
+    </svg>
+  `;
+}
+
+// Toogle Function for Like Button
+function toggleLike(index) {
+  books[index].liked = !books[index].liked;
+
+  if (books[index].liked) books[index].likes++;
+  else books[index].likes--;
+
+  renderBooks();
+}
+
+// Render Books on Page Load
+renderBooks();
